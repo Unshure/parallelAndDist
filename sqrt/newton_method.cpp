bd5a8de856a84ab29378e4aafbeffa429fdb6cd5
bbrucee <abae97@gmail.com>
2018-10-16 14:36:16 -0400

    Update newton_method.cpp
    Added comments

--- sqrt/newton_method.cpp
+++ sqrt/newton_method.cpp
@@ -18,6 +18,9 @@ extern void newton_method_serial(int input_size,
 
 void tester()
 {
+  // A function that runs each newton_method implementation on 8 different floats between 0-8
+  // Prints the value of each function and the built-in square root to provide a confirmation that all functions are computing the square root
+  // Likely would be better to compare the outputs and throw errors if they are not equal but this provides a visual confirmation that the following main isn't just wasting cycles for fun
   printf("Running tests for newton method functions \n");
   float input[8], output_serial[8], output_foreach[8], output_tasks[8];
   input[0] = 0.32423f;
@@ -39,41 +42,40 @@ void tester()
   }
   printf("Testing complete\n");
 }
+
 // Global due to being to large for the stack
 float input[20000000], output_serial[20000000], output_foreach[20000000], output_tasks[20000000];
 
 int main()
 {
   tester();
-  int array_size = 20000000; // change to 2000000;
-  // float input[array_size], output_serial[array_size], output_foreach[array_size], output_tasks[array_size];
+  int array_size = 20000000;
+  // Below used to work but had to be changed due to stack limitations
+  // float input[array_size], output_serial[array_size], output_foreach[array_size], output_tasks[array_size]; 
   for (int i=0; i<array_size; i++){
-      input[i] = 0.f + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(8)));
+      input[i] = 0.f + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(8))); // Random float between 0-8
   }
+  // Run and time each of the methods, printing the number of cycles spent inside each function call
+  
   printf("Running newton_method_serial\n");
   reset_and_start_timer();
-  
   newton_method_serial(array_size, 2.f, input, output_serial);
   double dt = get_elapsed_mcycles();
   printf("Complete after %.3f million cycles\n", dt);
 
-
   printf("Running newton_method_ispc\n");
   reset_and_start_timer();
-
   newton_method_ispc(array_size, 2.f, input, output_foreach);
   dt = get_elapsed_mcycles();
   printf("Complete after %.3f million cycles\n", dt);
 
-
   printf("Running newton_method_ispc_withtasks\n");
   reset_and_start_timer();
-
   newton_method_ispc_withtasks(array_size, 2.f, input, output_tasks);
   dt = get_elapsed_mcycles();
   printf("Complete after %.3f million cycles\n", dt);
-
-
-
+  
+  // Not yet implemented is a newton_method function using AVX intrinsics
+  
   return 0;
 }
