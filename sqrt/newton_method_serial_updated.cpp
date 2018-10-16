e623559093d5c7c56f6f42c34f6b130e6de8710e
bbrucee <abae97@gmail.com>
2018-10-16 14:30:25 -0400

    Update newton_method_serial_updated.cpp
    Just removed some print statements we used to debug

--- sqrt/newton_method_serial_updated.cpp
+++ sqrt/newton_method_serial_updated.cpp
@@ -17,21 +17,12 @@ void newton_method_serial(int input_size,
     for (int i=0; i<input_size; i++) {
 
         float S = input[i]; // value to find sqrt
-        //printf("S: %lf\n", S);
         float x_0 = x_initial;
-        //printf("x0: %lf\n",x_0);
         float x_1 = .5f * (x_0 + (S/x_0));
-        //printf("x1: %lf\n",x_1);
-
-
 
         while (fabs(x_1-x_0)> tolerance) {
             x_0 = x_1;
-            //printf("x0: %lf\n",x_0);
             x_1 = .5f * (x_0 + (S/x_0));
-            //printf("x1: %lf\n",x_1);
-
-
         }
 
         output[i] = x_1;
