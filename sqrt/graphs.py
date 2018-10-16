2c4b8071308d9be9b1a87735e36f96748a2b83ca
Atul <atulsrivastava3@yahoo.com>
2018-10-16 19:25:47 -0400

    added avx

---
+++ sqrt/graphs.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+thread_data = [[2,103.271], [3,94.412], [4,97.323], [5,84.133], [6,82.876], [7,77.136], [8,67.842]]
+print("this is a test")
+
+plt.plot(thread_data)
+plt.show()
