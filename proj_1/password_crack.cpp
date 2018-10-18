10050469f483060d649658d82b46782b997a8a42
Nick <nac542@gmail.com>
2018-10-18 03:05:42 -0400

    added parallel part 3

--- proj_1/password_crack.cpp
+++ proj_1/password_crack.cpp
@@ -20,13 +20,34 @@ char map(int convert){
   }
 }
 
+unsigned int RSHash(const char* str, size_t s)
+{
+    unsigned int b    = 378551;
+    unsigned int a    = 63689;
+    unsigned int hash = 0;
+
+    for(size_t i = 0; i < s; i++)
+    {
+        hash = hash * a + str[i];
+        a    = a * b;
+    }
+
+    return (hash & 0x7FFFFFFF);
+ }
+
+
 int main() {
 
-    string password = "bv37q";
+    //Create a file
 
-    int possibleLen = password.size();
+    char password[] = "bv3q5";
+
+    const int possibleLen = 5;
+
+    size_t h_pass = RSHash(password, possibleLen);
+
     //Set size is 36 characters and one blank character
-    int setSize = 36;
+    const int setSize = 36;
 
     //Create array of powers of setsize
     //Used later for setting guess characters
@@ -43,33 +64,42 @@ int main() {
     double duration;
     start = std::clock();
 
+    // Loop through len 1 - possible len
+    for (int currLen = 1; currLen <= possibleLen; ++currLen) {
     // Loop for all possible combinations
-    for (int currChar = 0; currChar <= (powers[possibleLen - 1] * setSize); ++currChar) {
-      string guess;
-      // Set guess
-      for (int guessIndex = 0; guessIndex < possibleLen; ++guessIndex) {
-        char temp = map((currChar / powers[guessIndex]) % setSize);
-        guess.append(1, temp);
-        if (temp == '\0') {
-          break;
+      for (int currChar = 0; currChar <= (powers[currLen - 1] * setSize ); ++currChar) {
+        char guess[5];
+        size_t h_guess;
+        // Set guess
+        for (int guessIndex = 0; guessIndex < currLen; ++guessIndex) {
+          char temp = map((currChar / powers[guessIndex]) % setSize);
+          guess[guessIndex] = temp;
+        }
+
+        //printf("Iteration: %d\tGuess: %s\n", currChar, guess);
+
+        // Check if it compares
+        h_guess = RSHash(guess, currLen);
+        if (h_pass == h_guess) {
+          printf("Match Found Single!! \nLen: %d\tIteration: %d\tGuess: %s\n",currLen, currChar, guess);
+          //break;
         }
       }
-
-      //printf("Iteration: %d\tGuess: %s\n", currChar, guess.c_str());
-
-      // Check if it compares
-      string check(guess);
-      if (password.compare(check) == 0) {
-        //printf("Cracked in %d guesses\n", currChar);
-        break;
-      }
     }
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    printf("Time: %f\n", duration);
+
+    
+    printf("-Starting Parallel Password Cracker-\n");
+
+    start = std::clock();
+
+    crack(h_pass, powers, possibleLen, setSize);
 
     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
     printf("Time: %f\n", duration);
 
-
-
+    
     return 0;
 }
 
